02c4b8e620515fc476fba0a9399cbc8d2b12f72f
Danny Tran <dtran@torontofilmschool.ca>
2024-10-10 11:40:24 -0400

    Basic Project Import

---
+++ Source/VGP221FallTerm2024/VGP221FallTerm2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VGP221FallTerm2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VGP221FallTerm2024, "VGP221FallTerm2024" );
